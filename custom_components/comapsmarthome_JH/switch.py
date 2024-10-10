24a7affd2eccbd524bb96ed625c65d5018da1b5e
jhenninot <51989690+jhenninot@users.noreply.github.com>
2024-10-10 18:00:36 +0200

    ajout switchs vacances et absence

--- custom_components/comapsmarthome_JH/switch.py
+++ custom_components/comapsmarthome_JH/switch.py
@@ -11,7 +11,7 @@ from homeassistant.helpers.device_registry import DeviceInfo
 from . import ComapClient, ComapCoordinator
 from .const import DOMAIN
 
-SCAN_INTERVAL = timedelta(minutes=5)
+SCAN_INTERVAL = timedelta(minutes=1)
 
 
 async def async_setup_entry(
@@ -21,10 +21,10 @@ async def async_setup_entry(
 ) -> None:
     config = hass.data[DOMAIN][config_entry.entry_id]
     client = ComapClient(username=config[CONF_USERNAME], password=config[CONF_PASSWORD])
-    async_add_entities([ComapHousingSensor(client)], update_before_add=True)
+    async_add_entities([ComapHousingOnOff(client),ComapHousingHoliday(client),ComapHousingAbsence(client)], update_before_add=True)
 
 
-class ComapHousingSensor(SwitchEntity):
+class ComapHousingOnOff(SwitchEntity):
     def __init__(self, client) -> None:
         super().__init__()
         self.client = client
@@ -60,7 +60,7 @@ class ComapHousingSensor(SwitchEntity):
     def is_on(self):
         """If the sensor is currently on or off."""
         return self._is_on
-
+    
     async def async_update(self):
         zones = await self.client.get_zones()
         self._is_on = zones["heating_system_state"] == "on"
@@ -70,3 +70,105 @@ class ComapHousingSensor(SwitchEntity):
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         return await self.client.turn_off()
+    
+class ComapHousingHoliday(SwitchEntity):
+    def __init__(self, client) -> None:
+        super().__init__()
+        self.client = client
+        self.housing = client.housing
+        housing = client.get_housings()[0]
+        self._name = "Holiday " + housing.get("name")
+        self._is_on = None
+        self._attr_device_class = SwitchDeviceClass.SWITCH
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device info."""
+        return DeviceInfo(
+            identifiers={
+                # Serial numbers are unique identifiers within a specific domain
+                (DOMAIN, self.unique_id)
+            },
+            name=self.name,
+            manufacturer="comap",
+        )
+
+    @property
+    def name(self) -> str:
+        """Return the name of the entity."""
+        return self._name
+
+    @property
+    def unique_id(self) -> str:
+        """Return the unique ID of the sensor."""
+        return self.client.housing + "_holiday"
+
+    @property
+    def is_on(self):
+        """If the sensor is currently on or off."""
+        return self._is_on
+
+    async def async_update(self):
+        zones = await self.client.get_zones()
+        events = zones.get("events")
+        if ('absence' in events):
+            self._is_on = True
+        else:
+            self._is_on = False
+       
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        return await self.client.set_holiday()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        return await self.client.delete_holiday()
+    
+class ComapHousingAbsence(SwitchEntity):
+    def __init__(self, client) -> None:
+        super().__init__()
+        self.client = client
+        self.housing = client.housing
+        housing = client.get_housings()[0]
+        self._name = "Absence " + housing.get("name")
+        self._is_on = None
+        self._attr_device_class = SwitchDeviceClass.SWITCH
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device info."""
+        return DeviceInfo(
+            identifiers={
+                # Serial numbers are unique identifiers within a specific domain
+                (DOMAIN, self.unique_id)
+            },
+            name=self.name,
+            manufacturer="comap",
+        )
+
+    @property
+    def name(self) -> str:
+        """Return the name of the entity."""
+        return self._name
+
+    @property
+    def unique_id(self) -> str:
+        """Return the unique ID of the sensor."""
+        return self.client.housing + "_absence"
+
+    @property
+    def is_on(self):
+        """If the sensor is currently on or off."""
+        return self._is_on
+
+    async def async_update(self):
+        zones = await self.client.get_zones()
+        events = zones.get("events")
+        if ('time_shift' in events):
+            self._is_on = True
+        else:
+            self._is_on = False
+       
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        return await self.client.set_absence()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        return await self.client.delete_absence()

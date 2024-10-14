331c7ecc565cbc7c729f321dd05f09975ea04697
jhenninot <51989690+jhenninot@users.noreply.github.com>
2024-10-14 14:02:11 +0200

    Ajout switch pour instructions temporaires

--- custom_components/comapsmarthome_JH/switch.py
+++ custom_components/comapsmarthome_JH/switch.py
@@ -23,8 +23,20 @@ async def async_setup_entry(
     client = ComapClient(username=config[CONF_USERNAME], password=config[CONF_PASSWORD])
     global HOUSING_DATA
     HOUSING_DATA = hass.data[DOMAIN]["housing"]
+    req = await client.get_zones()
+    zones = req.get("zones")
 
-    async_add_entities([ComapHousingOnOff(client),ComapHousingHoliday(client),ComapHousingAbsence(client)], update_before_add=True)
+    temporary_instructions_switches = [
+        ComapZoneTemporarySwitch(client, zone)
+        for zone in zones
+    ]
+
+    housing_switches = [ComapHousingOnOff(client),ComapHousingHoliday(client),ComapHousingAbsence(client)]
+    zones_switches = temporary_instructions_switches
+
+    switches = housing_switches + zones_switches
+
+    async_add_entities(switches, update_before_add=True)
 
 
 class ComapHousingOnOff(SwitchEntity):
@@ -172,3 +184,80 @@ class ComapHousingAbsence(SwitchEntity):
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         return await self.client.delete_absence()
+    
+
+class ComapZoneTemporarySwitch(SwitchEntity):
+    def __init__(self, client, zone) -> None:
+        super().__init__()
+        self.client = client
+        self.housing = HOUSING_DATA.get("id")
+        self._name = "Temporary " + HOUSING_DATA.get("name") + " " + zone.get("title")
+        self._id = zone.get("id") + "_temporary"
+        self.zone_name = zone.get("title")
+        self.zone_id = zone.get("id")
+        self._extra_state_attributes = {}
+        self._is_on = False
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device info."""
+        return DeviceInfo(
+            identifiers={
+                # Serial numbers are unique identifiers within a specific domain
+                (DOMAIN, self.zone_id)
+            },
+            name=self.zone_name,
+            manufacturer="comap",
+            serial_number = self.zone_id
+        )
+    
+    @property
+    def icon(self) -> str:
+        if self._is_on:
+            return "mdi:timer-minus"
+        return "mdi:timer-off"
+
+    @property
+    def name(self) -> str:
+        """Return the name of the entity."""
+        return self._name
+
+    @property
+    def unique_id(self) -> str:
+        """Return the unique ID of the sensor."""
+        return self._id
+    
+    @property
+    def extra_state_attributes(self):
+        # Retourne un dictionnaire d'attributs supplémentaires
+        return self._extra_state_attributes
+    
+    @property
+    def is_on(self):
+        """If the sensor is currently on or off."""
+        return self._is_on
+
+    async def async_update(self):
+        req = await self.client.get_zones()
+        zones = req.get("zones")
+        events = {}
+        for zone in zones:
+            if zone.get("id") == self.zone_id:
+                events = zone.get("events")
+        self._extra_state_attributes["temporary_instruction"] = events.get("temporary_instruction")
+        if ('temporary_instruction' in events):
+            self._is_on = True
+        else:
+            self._is_on = False
+       
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        return
+    
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        response = await self.client.remove_temporary_instruction(self.zone_id)
+        events = response.get("events")
+        self._extra_state_attributes["temporary_instruction"] = events.get("temporary_instruction")
+        if ('temporary_instruction' in events):
+            self._is_on = True
+        else:
+            self._is_on = False
